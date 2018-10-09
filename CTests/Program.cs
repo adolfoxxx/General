1622cdeaedd8e2c0d4ae64d409501edf83c45b44
adolfoxxx <adolfo.molina@fox.com>
2018-10-09 12:19:02 -0600

    Testing Slack notifications.

--- CTests/Program.cs
+++ CTests/Program.cs
@@ -7,7 +7,6 @@ namespace myApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
         }
     }
 }
